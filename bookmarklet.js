80b1e01776cc33c97ffdaf754ead7ac415c9dc8e
Petros Kolyvas <code@petros.io>
2023-06-10 12:24:02 -0300

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,7 @@ javascript: (function() {
     var isReddit = document.location.hostname.split('.').slice(-2).join('.') === 'reddit.com';
     var isOverview = document.location.href.match(/\/overview\b/);
     if (isReddit && isOverview) {
-        var cachBustUrl = 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
+        var cachBustUrl = 'https://raw.githubusercontent.com/pkolyvas/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
         fetch(cachBustUrl).then(function(response) {
             return response.text();
         }).then(function(data) {
